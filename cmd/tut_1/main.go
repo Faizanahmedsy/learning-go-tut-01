806dc129a49f06671530a8e16090b30e0545142f
Faizanahmed Saiyed <119733178+Faizanahmedsy@users.noreply.github.com>
2024-02-01 07:37:26 +0530

    foo

--- cmd/tut_1/main.go
+++ cmd/tut_1/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"unicode/utf8"
+)
 
 
 
@@ -9,6 +12,16 @@ func main(){
 	fmt.Println("Hello World")
 	var num int16 = 10;
 	fmt.Println(num)
+	
 	var sayHey string  = "Hello \nWorld"
 	fmt.Println(sayHey)
+
+	const PI float64 = 3.1415
+	fmt.Println(PI)
+
+	// const thisWillNotFindLengthOFTheString = len('test')
+
+	fmt.Println(len("faizan")) // This will output number of bytes of the string and not the actual length of the string
+
+	fmt.Println(utf8.RuneCountInString("Faizan")) // This will output the length of the string
 }
