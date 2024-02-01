27c593477e70c7a2885dd8b01d0b19d0d457f5fe
Faizanahmed Saiyed <119733178+Faizanahmedsy@users.noreply.github.com>
2024-02-01 07:28:56 +0530

    Initial COmmit

---
+++ cmd/tut_1/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+
+
+
+func main(){
+	fmt.Println("Hello World")
+	var num int16 = 10;
+	fmt.Println(num)
+	var sayHey string  = "Hello \nWorld"
+	fmt.Println(sayHey)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module my_first_go_program
+
+go 1.21.6
